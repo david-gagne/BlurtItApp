3387848a944a18021d85bb6660583d3295382443
David Gagne <buildpax@gmail.com>
2022-09-23 20:20:16 -0400

    Added conditional logic for iOS functionality (w/ help of manifest file settings)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
+// Step 1. Define the variables for the elements.
+let input = document.getElementById("message");
+let button = document.getElementById("trigger-button");
+let fullscreenContainer = document.getElementById("fullscreen-container");
+
 // Check if Fullscreen API is available
 if (document.fullscreenEnabled) {
-  // Step 1. Define the variables for the elements.
-  let input = document.getElementById("message");
-  let button = document.getElementById("trigger-button");
-  let fullscreenContainer = document.getElementById("fullscreen-container");
-
   // Reveal or hide button based on whether user has typed a message
   message.addEventListener("keyup", function (event) {
     if (event.target.value.length > 0) {
@@ -35,10 +35,39 @@ if (document.fullscreenEnabled) {
     document.exitFullscreen();
   });
 } else {
-  // let body = document.querySelector("body");
-  // let unsupportedMessage = document.getElementById(
-  //   "unsupported-message-container"
-  // );
-  // body.classList.add("overflow-hidden");
-  // unsupportedMessage.classList.replace("dn", "flex");
+  // Reveal or hide button based on whether user has typed a message
+  message.addEventListener("keyup", function (event) {
+    if (event.target.value.length > 0) {
+      button.removeAttribute("disabled");
+    } else {
+      button.setAttribute("disabled", "");
+    }
+  });
+
+  button.addEventListener("click", function () {
+    let blurt = document.getElementById("blurt-message");
+
+    // Display the off-canvas message container element.
+    fullscreenContainer.classList.remove("dn");
+    fullscreenContainer.classList.add(
+      "flex",
+      "vh-100",
+      "z-999",
+      "overflow-hidden"
+    );
+
+    // Set input value to blurt message
+    blurt.textContent = input.value;
+  });
+
+  fullscreenContainer.addEventListener("click", function () {
+    // Remove the off-canvas message container element.
+    fullscreenContainer.classList.remove(
+      "flex",
+      "vh-100",
+      "z-999",
+      "overflow-hidden"
+    );
+    fullscreenContainer.classList.add("dn");
+  });
 }
